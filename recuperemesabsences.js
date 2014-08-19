7951a257d935c03fd8a03d91ccee1884f594ca9d
Legrand Benjamin <dev@legrand.ws>
2014-08-19 14:00:26 +0200

    Dates are done with the timezone included. When converting to ISO we should keep the included timezone and not use the UTC date. Then send the time as a UTC date. This is kind of weird way of doing things but never mind.\n Bump version number

--- recuperemesabsences.js
+++ recuperemesabsences.js
@@ -34,12 +34,12 @@ function dateToYMD(date) {
 
 function ISODateString(d){
  	function pad(n){return n<10 ? '0'+n : n}
- 	return d.getUTCFullYear()+'-'
-      	+ pad(d.getUTCMonth()+1)+'-'
-      	+ pad(d.getUTCDate())+'T'
-      	+ pad(d.getUTCHours())+':'
-      	+ pad(d.getUTCMinutes())+':'
-      	+ pad(d.getUTCSeconds());
+ 	return d.getFullYear()+'-'
+      	+ pad(d.getMonth()+1)+'-'
+      	+ pad(d.getDate())+'T'
+      	+ pad(d.getHours())+':'
+      	+ pad(d.getMinutes())+':'
+      	+ pad(d.getSeconds());
 }
 
 function UpdateCalendar(calendar){
